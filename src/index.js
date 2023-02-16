312068cd0720fdc8dc708620421f1b07ccef8453
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-02-16 09:25:31 +0200

    2

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
