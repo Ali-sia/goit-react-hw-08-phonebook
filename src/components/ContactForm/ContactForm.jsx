6a9047efa425cd4baf7c6ea2268ac7d5cc0e91a2
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-02-20 18:50:01 +0200

    add style for "add contact" section

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -8,6 +8,11 @@ import { toast } from 'react-hot-toast';
 // import PropTypes from 'prop-types';
 import { Box } from '../Box';
 import { EnterLabel, EnterInput, StyledButton } from '../App.styled';
+
+import FormGroup from '@mui/material/FormGroup';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 import { nanoid } from '../../../node_modules/nanoid/index';
 
 const ContactForm = () => {
@@ -28,6 +33,14 @@ const ContactForm = () => {
       number,
     };
 
+    // //if fields are empty
+    // if (name.trim() || number) {
+    //   toast.error(`${name} is already in contacts`, {
+    //     autoClose: 1500,
+    //   });
+    //   return;
+    // }
+
     // if contact already exist
     if (contacts.find(contact => contact.name === newContact.name)) {
       toast.error(`${name} is already in contacts`, {
@@ -46,40 +59,82 @@ const ContactForm = () => {
   };
 
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      alignItems="flex-start"
-      as="form"
-      onSubmit={handleSubmit}
-    >
-      <EnterLabel>
-        Name:
-        <EnterInput
+    <form onSubmit={handleSubmit}>
+      <FormGroup
+        sx={{
+          border: '1px solid gray',
+          borderRadius: '8px',
+          padding: '10px',
+          marginRight: 'auto',
+          marginLeft: 'auto',
+          // backgroundColor: 'lightgray',
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Contact name"
+          variant="outlined"
           type="text"
           name="name"
           value={name}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
           onChange={handleChangeName}
+          required
+          sx={{
+            marginBottom: '20px',
+          }}
         />
-      </EnterLabel>
-
-      <EnterLabel>
-        Number:
-        <EnterInput
+        <TextField
+          id="outlined-basic"
+          label="Email"
+          variant="outlined"
           type="tel"
           name="number"
-          value={number}
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
           onChange={handleChangeNumber}
+          required
+          sx={{
+            marginBottom: '20px',
+          }}
         />
-      </EnterLabel>
-      <StyledButton type="submit">Add contact</StyledButton>
-    </Box>
+
+        <Button variant="outlined">Add contact</Button>
+      </FormGroup>
+    </form>
+    // <Box
+    //   display="flex"
+    //   flexDirection="column"
+    //   alignItems="flex-start"
+    //   as="form"
+    //   onSubmit={handleSubmit}
+    // >
+    //   <EnterLabel>
+    //     Name:
+    //     <EnterInput
+    //       type="text"
+    //       name="name"
+    //       value={name}
+    //       pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+    //       title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    //       required
+    //       onChange={handleChangeName}
+    //     />
+    //   </EnterLabel>
+
+    //   <EnterLabel>
+    //     Number:
+    //     <EnterInput
+    //       type="tel"
+    //       name="number"
+    //       value={number}
+    //       pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+    //       title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+    //       required
+    //       onChange={handleChangeNumber}
+    //     />
+    //   </EnterLabel>
+    //   <StyledButton type="submit">Add contact</StyledButton>
+    // </Box>
   );
 };
 
