074525abdd53273c561ebffe69a1661bad5d432e
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-02-20 21:41:37 +0200

    contact form styles

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -6,12 +6,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import { toast } from 'react-hot-toast';
 
 // import PropTypes from 'prop-types';
-import { Box } from '../Box';
-import { EnterLabel, EnterInput, StyledButton } from '../App.styled';
-
 import FormGroup from '@mui/material/FormGroup';
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+import { ColorButton } from 'components/ButtonStyledFromMUI';
 
 import { nanoid } from '../../../node_modules/nanoid/index';
 
@@ -33,14 +33,6 @@ const ContactForm = () => {
       number,
     };
 
-    // //if fields are empty
-    // if (name.trim() || number) {
-    //   toast.error(`${name} is already in contacts`, {
-    //     autoClose: 1500,
-    //   });
-    //   return;
-    // }
-
     // if contact already exist
     if (contacts.find(contact => contact.name === newContact.name)) {
       toast.error(`${name} is already in contacts`, {
@@ -54,93 +46,58 @@ const ContactForm = () => {
       autoClose: 1500,
     });
 
-    setName('');
-    setNumber('');
+    setName(' ');
+    setNumber(' ');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <FormGroup
-        sx={{
-          border: '1px solid gray',
-          borderRadius: '8px',
-          padding: '10px',
-          marginRight: 'auto',
-          marginLeft: 'auto',
-          // backgroundColor: 'lightgray',
-        }}
-      >
-        <TextField
-          id="outlined-basic"
-          label="Contact name"
-          variant="outlined"
-          type="text"
-          name="name"
-          value={name}
-          onChange={handleChangeName}
-          required
+    <Box sx={{ width: '100%' }}>
+      <Typography variant="h4" gutterBottom>
+        Add contact
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <FormGroup
           sx={{
-            marginBottom: '20px',
+            marginRight: 'auto',
+            marginLeft: 'auto',
           }}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          onChange={handleChangeNumber}
-          required
-          sx={{
-            marginBottom: '20px',
-          }}
-        />
+        >
+          <TextField
+            id="outlined-basic"
+            label="Contact name"
+            variant="outlined"
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleChangeName}
+            required
+            sx={{
+              marginBottom: '20px',
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Number"
+            variant="outlined"
+            type="tel"
+            name="number"
+            value={number}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            onChange={handleChangeNumber}
+            required
+            sx={{
+              marginBottom: '20px',
+            }}
+          />
 
-        <Button variant="outlined">Add contact</Button>
-      </FormGroup>
-    </form>
-    // <Box
-    //   display="flex"
-    //   flexDirection="column"
-    //   alignItems="flex-start"
-    //   as="form"
-    //   onSubmit={handleSubmit}
-    // >
-    //   <EnterLabel>
-    //     Name:
-    //     <EnterInput
-    //       type="text"
-    //       name="name"
-    //       value={name}
-    //       pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-    //       title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-    //       required
-    //       onChange={handleChangeName}
-    //     />
-    //   </EnterLabel>
-
-    //   <EnterLabel>
-    //     Number:
-    //     <EnterInput
-    //       type="tel"
-    //       name="number"
-    //       value={number}
-    //       pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-    //       title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-    //       required
-    //       onChange={handleChangeNumber}
-    //     />
-    //   </EnterLabel>
-    //   <StyledButton type="submit">Add contact</StyledButton>
-    // </Box>
+          <ColorButton variant="contained" type="submit">
+            Add contact
+          </ColorButton>
+        </FormGroup>
+      </form>
+    </Box>
   );
 };
-
-// ContactForm.propTypes = {
-//   name: PropTypes.string,
-//   number: PropTypes.string,
-// };
 
 export default ContactForm;
